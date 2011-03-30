0ace081e6a2fda4103a0be011f871273380a1c3b
Anthony Goddard <goddardanthony@gmail.com>
2011-03-30 17:47:13 -0400

    creating lsyncd cookbook

---
+++ lsyncd/recipes/default.rb
@@ -0,0 +1,28 @@
+#
+# Cookbook Name:: lsyncd
+# Recipe:: default
+#
+# Copyright 2011, Woods Hole Marine Biological Laboratory
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# == Client install
+# 
+# Ubuntu (lsyncd 2.0.x)
+# apt-get install lua5.1 liblua5.1-0-dev liblua50-dev liblualib50-dev
+# 
+# wget http://lsyncd.googlecode.com/files/lsyncd-2.0.3.tar.gz
+# tar -zxf lsyncd-2.0.3.tar.gz 
+# cd lsyncd-2.0.3.tar.gz
+# ./configure
