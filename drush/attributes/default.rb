bc6cf1b7c717b3457e4b0fe9c0b6187b4a3bac51
adorsk-whoi <adorsk@whoi.edu>
2011-05-13 09:16:04 -0400

    changed default drush version

--- drush/attributes/default.rb
+++ drush/attributes/default.rb
@@ -3,7 +3,8 @@
 # Attributes:: drush
 #
 
-default[:drush][:version] = "4.2"
-default[:drush][:base_url] = "http://ftp.drupal.org/files/projects/"
-default[:drush][:checksum] = "6ab024cdec530223a008fb3ec3e5a94f478c8a999038ffc181561fbb4e7c3872"
+default[:drush][:version] = "4.4"
+default[:drush][:base_url] = "http://ftp.drupal.org/files/projects/drush-\
+7.x-"
+default[:drush][:checksum] = "b8f89ee75a8d45a4765679524ebdf8b4",
 default[:drush][:dir] = "/usr/local/drush"
