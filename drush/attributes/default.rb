37007e35cc64a7e726e5870de956895630429022
adorsk-whoi <adorsk@whoi.edu>
2011-05-13 08:54:29 -0400

    moved drush to its own recipe

---
+++ drush/attributes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: drush
+# Attributes:: drush
+#
+
+default[:drush][:version] = "4.2"
+default[:drush][:base_url] = "http://ftp.drupal.org/files/projects/"
+default[:drush][:checksum] = "6ab024cdec530223a008fb3ec3e5a94f478c8a999038ffc181561fbb4e7c3872"
+default[:drush][:dir] = "/usr/local/drush"
--- drush/recipes/default.rb
+++ drush/recipes/default.rb
@@ -1,24 +1,6 @@
 #
-# Author:: Marius Ducea (marius@promethost.com)
-# Cookbook Name:: drupal
-# Recipe:: drush
-#
-# Copyright 2010, Promet Solutions
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-include_recipe %w{php::mysql}
+# Cookbook Name:: drush
+# Recipe:: default
 
 drush_file_url = "#{node[:drush][:base_url]}#{node[:drush][:version]}.tar.gz"
 local_drush_file = "/tmp/drush.tar.gz"
