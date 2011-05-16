262ade5acf8c4b8caad951f1090d889ec5cd4863
adorsk-whoi <adorsk@whoi.edu>
2011-05-16 10:56:54 -0400

    fixed typo in drush attributes

--- drush/attributes/default.rb
+++ drush/attributes/default.rb
@@ -4,7 +4,6 @@
 #
 
 default[:drush][:version] = "4.4"
-default[:drush][:base_url] = "http://ftp.drupal.org/files/projects/drush-\
-7.x-"
+default[:drush][:base_url] = "http://ftp.drupal.org/files/projects/drush-7.x-"
 default[:drush][:checksum] = "b8f89ee75a8d45a4765679524ebdf8b4",
 default[:drush][:dir] = "/usr/local/drush"
--- php/attributes/cli.rb
+++
@@ -1,7 +0,0 @@
-#
-# Cookbook Name:: php
-# Attribute:: cli
-#
-
-# Set the php cli memory limit.
-default_unless['php']['cli']['memory_limit'] = '-1'
