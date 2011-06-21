8f71eeb6c1d47717e1712bbb05bb27cd673de053
Anthony Goddard <agoddard@mbl.edu>
2011-06-21 16:19:01 -0400

    added deb-backports to apt

---
+++ apt/recipes/backports.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: apt
+# Recipe:: default
+#
+# Copyright 2011, Woods Hole Marine Biological Laboratory.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+apt_repository "backports" do
+  uri "http://backports.debian.org/debian-backports"
+  components ["lenny-backports","main","contrib","non-free"]
+  keyserver "pgp.mit.edu"
+  key "AED4B06F473041FA"
+  action :add
+end
