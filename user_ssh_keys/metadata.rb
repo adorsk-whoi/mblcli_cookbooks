8341fbad8576a90ed483f34843535f4e4e9923f0
adorsk-whoi <adorsk@whoi.edu>
2011-06-27 15:26:21 -0400

    added user_ssh_keys cookbook

---
+++ user_ssh_keys/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "adorsk-whoi"
+maintainer_email "adorsk@whoi.edu"
+license          "All rights reserved"
+description      "Assign SSH keys stored in databags to users."
+version          "0.0.1"
+
