ee30f11afb8aee34511006d2c3e59b89a8071e63
adorsk-whoi <adorsk@whoi.edu>
2011-07-19 09:39:12 -0400

    changed name of user_ssh_keys recipe to set_user_ssh_keys

--- user_ssh_keys/attributes/default.rb
+++
@@ -1,6 +0,0 @@
-#
-# Cookbook Name:: user_ssh_keys
-# Attributes:: default
-#
-
-default['user_ssh_keys']['databags'] = []
--- user_ssh_keys/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "adorsk-whoi"
-maintainer_email "adorsk@whoi.edu"
-license          "All rights reserved"
-description      "Assign SSH keys stored in databags to users."
-version          "0.0.1"
-
