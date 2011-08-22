ebfded350fb3ed0061c8471f899b5e7363d1397b
adorsk-whoi <adorsk@whoi.edu>
2011-08-22 14:20:44 -0400

    changed method of checking for defunct files to accoutn for user name changes

--- whenever/recipes/default.rb
+++ whenever/recipes/default.rb
@@ -49,20 +49,11 @@ directory "#{node['whenever']['configs_dir']}" do
   action :create
 end
 
-# Get list of existing whenever job files.
-existing_job_files = `find #{node['whenever']['configs_dir']} -type f`.map{|l| l.strip}
+# Get list of existing old whenever job files.
+old_job_files = `find #{node['whenever']['configs_dir']} -type f`.map{|l| l.strip}
 
-# Remove defunct jobs which are not in the node attributes.
-existing_job_files.each do |job_file|
-
-  job_id = File.basename(job_file, File.extname(job_file))
-
-  if ! node['whenever']['jobs'][job_id]
-    execute "remove defunct whenever job '#{job_id}'" do
-      command "whenever -c -f #{job_file}; rm -f #{job_file}"
-    end    
-  end
-end
+# Initialize hash of current job files (current per node's attributes).
+current_job_files = {}
 
 # Process each job attribute.
 node["whenever"]["jobs"].each do |job_id, job|
@@ -77,22 +68,45 @@ node["whenever"]["jobs"].each do |job_id, job|
   # Get 'user'.
   user = job['user'] || node['whenever']['defaults']['user']
 
-  # Path to temporary whenever config file.
-  whenever_file = "#{node['whenever']['configs_dir']}/#{job_id}.rb"
+  # Path to whenever schedule file that will be written or updated.
+  # Note: filename convention is '_job_job_id-_user_username'.  We use this convention
+  # in order to detect changes in job user attributes.
+  whenever_file = "#{node['whenever']['configs_dir']}/__job:#{job_id}__user:#{user}"
   
   # Write job file and notify execution of whenever update command.
-  template "#{whenever_file}" do
+  template whenever_file do
     source "whenever_job.erb"
     group "root"
     owner "root"
     variables(:command => job['command'], :every => every, :at => at)
     mode 0644
-    notifies :run, "execute[update whenever job '#{job_id}']"
+    notifies :run, "execute[update whenever job '#{whenever_file}']"
   end
 
   # Update crontab from whenever job files.
-  execute "update whenever job '#{job_id}'" do
-    command "whenever -i -f #{whenever_file}"
+  execute "update whenever job '#{whenever_file}'" do
+    command "whenever -u #{user} -i -f #{whenever_file}"
     action :nothing
+  end
+
+  # Save whenever file to hash of new job files.
+  current_job_files[whenever_file] = true
+
+end
+
+
+# Remove old jobs which are not in current jobs.
+old_job_files.each do |job_file|
+
+  if ! current_job_files[job_file]
+
+    # Extract the user and job_id from the job file name.
+    if File.basename(job_file) =~ /^__job:(.*)__user:(.*)$/
+      user = $2
+      execute "remove defunct whenever job '#{job_file}'" do
+        command "whenever -u #{user} -c -f #{job_file}; rm -f #{job_file}"
+      end    
+    end
+
   end
 end
