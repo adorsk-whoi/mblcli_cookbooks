cb6f990684522bee6181715de5db4b66242c6b18
adorsk-whoi <adorsk@whoi.edu>
2011-08-23 10:59:08 -0400

    added error checking for bad whenever job names (containing slashes)

--- whenever/resources/job.rb
+++ whenever/resources/job.rb
@@ -1,6 +1,6 @@
 actions :create, :delete
 
-attribute :name, :kind_of => String, :name_attribute => true, :required => true
+attribute :name, :kind_of => String, :name_attribute => true, :required => true, :regex => /^[^\/]+$/
 attribute :every, :kind_of => String
 attribute :at, :kind_of => String
 attribute :user, :kind_of => String
