897a1e07e2902fa2967771373e8582765c0f2f79
adorsk-whoi <adorsk@whoi.edu>
2011-08-24 09:55:26 -0400

    removed defunct ohai-plugins cookbook

--- ohai-plugins/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer       "Marine Biological Laboratory"
-maintainer_email "agoddard@mbl.edu"
-license           "Apache 2.0"
-description       "Installs ohai plugins"
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version           "0.0.1"
-
-recipe "ohai-plugins", "installs ohai plugins"
